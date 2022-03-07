8a876ecaa14d37f7084db51694faa3ce5aca9d62
raviramani95 <ravi.ramani@oneadvanced.com>
2022-03-07 14:06:36 +0530

    17-242 angular pipes start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,48 +6,37 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') submitForm: NgForm;
-  submitted = false;
-  genders = ['male','female'];
-  answer: string = '';
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: ''
-  };
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    // this.submitForm.setValue({
-    //   userData: {
-    //     username: suggestedName,
-    //     email: ''
-    //   },
-    //   secret: 'pet',
-    //   questionAnswer: '',
-    //   gender: 'male'
-    // });
-    this.submitForm.form.patchValue({
-      userData: {
-        username: suggestedName
-      }
-    });
-  }
-
-  // onSubmit(form: NgForm){
-  //   console.log(form);
-  // }
-  onSubmit(){
-    console.log(this.submitForm);
-    this.submitted = true;
-    this.user.username = this.submitForm.value.userData.username;
-    this.user.email = this.submitForm.value.userData.email;
-    this.user.secretQuestion = this.submitForm.value.userData.secretQuestion;
-    this.user.answer = this.submitForm.value.userData.questionAnswer;
-    this.user.gender = this.submitForm.value.userData.gender;
-
-    this.submitForm.reset();
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    }
+  ];
+  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
   }
 }
